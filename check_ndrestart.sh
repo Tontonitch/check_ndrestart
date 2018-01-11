f1af8ffa3cd9354e9d99f2050e381e10b2dbc454
Yannick Charton <tontonitch-pro@yahoo.fr>
2018-01-11 14:05:31 +0100

    Add exclude list feature
    Add exclude list feature. Issue #3

--- check_ndrestart.sh
+++ check_ndrestart.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #set -x
 # ========================================================================================
-# System/services restart need check plugin for Nagios/Icinga
+# Needs-restarting for Nagios
 #
 # Written by    : Yannick Charton
-# Release       : 1.0
-# Creation date : 8 May 2017
-# Description   : Nagios plugin (script) to if the system needs to be restarted or if some 
-#               processes/services need to be restarted following some package updates.
+# Description   : Nagios plugin (script) to if the system needs to be restarted or if some
+#                 processes/services need to be restarted following some package updates.
 #
-# Usage         : ./check_ndrestart.sh
+# Usage         : ./check_ndrestart.sh [-e service_to_exclude] [-e service_to_exclude]"
 #
 # Installation  : some sudo settings need to be configured. Add a file /etc/sudoers.d/check_ndrestart
 #                 containing:
@@ -21,9 +19,13 @@
 # HISTORY :
 #     Release   |     Date      |    Authors    |       Description
 # --------------+---------------+---------------+------------------------------------------
+#       1.1     |    10.01.18   | Y. Charton    | Exclude list
+# --------------+---------------+---------------+------------------------------------------
 #       1.0     |    08.05.17   | Y. Charton    | Initial release
 # --------------+---------------+---------------+------------------------------------------
-#
+# 
+# Notes: needs-restarting can produce false-positive reports for applications behaving "abnormally"
+#        see https://access.redhat.com/solutions/3317951 for an Oracle Clusterware process case
 # =========================================================================================
 
 # Paths to commands used in this script.  These may have to be modified to match your system setup.
@@ -38,8 +40,14 @@ STATUS=$STATE_OK
 
 # Plugin variable description
 PROGNAME=$(basename $0)
-RELEASE="Revision 1.0"
+RELEASE="Revision 1.1"
 AUTHOR="by Yannick Charton"
+
+# Other variables
+EXCLUDE=""
+NEEDREBOOT=""
+RC_NEEDREBOOT=0
+NEEDSRVRESTART=""
 
 if [ ! -x $LSBRELEASE ]; then
         echo "UNKNOWN: lsb_release not found or is not executable by the nagios/icinga user."
@@ -54,9 +62,9 @@ print_release() {
 print_usage() {
     echo ""
     echo "$PROGNAME $RELEASE - Nagios plugin (script) to if the system needs to be restarted"
-	echo "or if some processes/services need to be restarted following some package updates."
+        echo "or if some processes/services need to be restarted following some package updates."
     echo ""
-    echo "Usage: check_ndrestart.sh"
+    echo "Usage: check_ndrestart.sh [-e service_to_exclude] [-e service_to_exclude]"
     echo ""
     echo "Usage: $PROGNAME"
     echo "Usage: $PROGNAME --help"
@@ -64,60 +72,89 @@ print_usage() {
 }
 
 print_help() {
-        print_usage
-        echo ""
-        echo "This plugin will check if the system needs to be restarted or if some"
-	    echo "processes/services need to be restarted following some package updates."
-        echo ""
-        exit 0
+    print_usage
+    echo ""
+    echo "This plugin will check if the system needs to be restarted or if some"
+    echo "processes/services need to be restarted following some package updates."
+    echo ""
+    exit 0
 }
 
-# 
+# Parse parameters
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h | --help)
+            print_help
+            exit $STATE_OK
+            ;;
+        -v | --version)
+            print_release
+            exit $STATE_OK
+            ;;
+        -e | --exclude)
+            shift
+            if [ -n "${EXCLUDE}" ]; then EXCLUDE="${EXCLUDE}|"; fi
+                        EXCLUDE="${EXCLUDE}$1"
+            ;;
+        *)  echo "Unknown argument: $1"
+            print_usage
+            exit $STATE_UNKNOWN
+            ;;
+    esac
+shift
+done
+
+
+# Distrib dependent commands
 case `uname` in
         Linux ) LSBR_DISTRID=`lsb_release -i -s`
-                LSBR_DISTRRN=`lsb_release -r -s | cut -f1,2 -d'.'`
+                LSBR_DISTRRN=`lsb_release -r -s`
             ;;
         *)      echo "UNKNOWN: `uname` not yet supported by this plugin. Coming soon !"
                 exit $STATE_UNKNOWN
             ;;
 esac
 
-NEEDREBOOT=""
-RC_NEEDREBOOT=0
-NEEDSRVRESTART=""
-RC_NEEDSRVRESTART=0
 
 case $LSBR_DISTRID in
-    RedHatEnterpriseServer | CentOS) 
-	  if [ $(bc <<< "$LSBR_DISTRRN >= 7.3") -ne 0 ]; then 
-	    NEEDREBOOT=$(needs-restarting -r 2>&1)
-		RC_NEEDREBOOT=$?
-		NEEDSRVRESTART=$(sudo needs-restarting -s 2>&1)
-		[ "$(echo $NEEDSRVRESTART)" != "" ] && RC_NEEDSRVRESTART=1
-      else
-	    NEEDSRVRESTART=$(sudo needs-restarting 2>&1)
-		RC_NEEDSRVRESTART=$?
-      fi
-	  ;;
+    RedHatEnterpriseServer | CentOS)
+        if [ $(bc <<< "$LSBR_DISTRRN >= 7.3") -ne 0 ]; then
+            NEEDREBOOT=$(needs-restarting -r 2>&1)
+            RC_NEEDREBOOT=$?
+
+            if [ -n "${EXCLUDE}" ]; then
+                NEEDSRVRESTART=$(sudo needs-restarting -s 2>&1 | egrep -v "${EXCLUDE}")
+                        else
+                NEEDSRVRESTART=$(sudo needs-restarting -s 2>&1)
+            fi
+
+        else
+            if [ -n "${EXCLUDE}" ]; then
+                NEEDSRVRESTART=$(sudo needs-restarting 2>&1 | egrep -v "${EXCLUDE}")
+                        else
+                NEEDSRVRESTART=$(sudo needs-restarting 2>&1)
+            fi
+        fi
+        ;;
     *)
       echo "UNKNOWN: `uname` not yet supported by this plugin. Coming soon !"
       exit $STATE_UNKNOWN
       ;;
-esac		
+esac
 
 if [ "${RC_NEEDREBOOT}" != "0" ]; then
-        MSG_WARN="${MSG_WARN} Core libraries or services have been updated, reboot is required."
-		EXTRA_WARN="${EXTRA_WARN}
+    MSG_WARN="${MSG_WARN} Core libraries or services have been updated, reboot is required."
+    EXTRA_WARN="${EXTRA_WARN}
 $NEEDREBOOT"
-		STATUS=$STATE_WARNING
+    STATUS=$STATE_WARNING
 fi
 
-if [ "${RC_NEEDSRVRESTART}" != "0" ]; then
-        MSG_WARN="${MSG_WARN} Some services need to be restarted."
-		EXTRA_WARN="${EXTRA_WARN}
+if [ -n  "${NEEDSRVRESTART}" ]; then
+    MSG_WARN="${MSG_WARN} Some services need to be restarted."
+    EXTRA_WARN="${EXTRA_WARN}
 Services that need to be restarted:
 ${NEEDSRVRESTART}"
-		STATUS=$STATE_WARNING
+    STATUS=$STATE_WARNING
 fi
 
 if [ $STATUS -eq 1 ]; then
@@ -125,4 +162,3 @@ if [ $STATUS -eq 1 ]; then
 else
     echo "OK: No processes need to be restarted, no reboot required";
 fi
-exit $STATUS
