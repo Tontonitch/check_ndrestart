85c8d2e5086af5a302c0031af93e63a49f6ea5af
Yannick Charton <tontonitch-pro@yahoo.fr>
2018-08-20 11:24:01 +0200

    Add info about the package providing lsb_release

--- check_ndrestart.sh
+++ check_ndrestart.sh
@@ -51,6 +51,7 @@ NEEDSRVRESTART=""
 
 if [ ! -x $LSBRELEASE ]; then
         echo "UNKNOWN: lsb_release not found or is not executable by the nagios/icinga user."
+        echo "If the binary doesn't exist, you need to look for the package redhat-lsb-core."
         exit $STATE_UNKNOWN
 fi
 
